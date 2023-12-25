567575382a658d76e8d57a0b62ede79a98e40863
Stanislav Romanov <stasromanov801@gmail.com>
2023-12-25 06:01:32 +0100

    Composing complex interfaces -> done

---
+++ Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Stanislav Romanov on 25.12.2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+  @EnvironmentObject var modelData: ModelData
+  
+  var body: some View {
+    NavigationSplitView {
+      List {
+        modelData.features[0].image
+          .resizable()
+          .scaledToFill()
+          .frame(height: 200)
+          .clipped()
+          .listRowInsets(EdgeInsets())
+        
+        ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+          CategoryRow(categoryName: key, items: modelData.categories[key]!)
+        }
+        .listRowInsets(EdgeInsets())
+      }
+      .navigationTitle("Featured")
+    } detail: {
+      Text("Select a Landmark")
+    }
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+  static var previews: some View {
+    CategoryHome()
+      .environmentObject(ModelData())
+  }
+}
---
+++ Landmarks/View/Categories/CategoryRow.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Stanislav Romanov on 25.12.2023.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+  var categoryName: String
+  var items: [Landmark]
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(categoryName)
+        .font(.headline)
+        .padding(.leading, 15)
+        .padding(.top, 5)
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top, spacing: 0.0) {
+          ForEach(items) { item in
+            NavigationLink {
+              LandmarkDetail(landmark: item)
+            } label: {
+              CategoryItem(landmark: item)
+            }
+          }
+        }
+      }
+      .frame(height: 185.0)
+    }
+  }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+  static var previews: some View {
+    let landmarks = ModelData().landmarks
+    CategoryRow(categoryName: landmarks[0].category.rawValue,
+                items: Array(landmarks.prefix(3)))
+  }
+}

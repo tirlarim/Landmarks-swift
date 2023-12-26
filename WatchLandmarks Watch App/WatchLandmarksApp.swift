dbbc86a39dc6db966fe07925034af7a2e24704bf
Stanislav Romanov <stasromanov801@gmail.com>
2023-12-26 13:24:14 +0100

    add watch

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Stanislav Romanov on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
